6722d4fd33320d5da9971ac0dd91fa1b9ded2919
erkan011 <erkancaliskan011@gmail.com>
2024-03-05 19:10:07 +0300

    Null değeri, bir değişkenin hiçbir değere sahip olmadığını ifade eder. Programlama dillerinde null genellikle aşağıdaki durumlarda kullanılır:
        Değişkenlerin başlangıç değerleri: Değişkenlerin başlangıçta bir değere sahip olmayabileceği durumlar için null kullanılabilir. Örneğin, bir kullanıcının profili oluşturulurken belirli bilgilerin eksik olabileceği durumlarda bu bilgileri null olarak işaretleyebilirsiniz.
    
        Metotlardan veya fonksiyonlardan dönüş değerleri: Bir fonksiyonun veya metodun belirli bir koşul altında değer döndürmesi gerekmeyebilir. Bu durumda, null döndürerek bir şeyin dönmediğini belirtebilirsiniz. Örneğin, bir veritabanından bir kullanıcıyı arayan bir fonksiyon, belirli bir kullanıcıyı bulamadığında null döndürebilir.
    
        Veritabanı işlemleri: Bir veritabanından belirli bir kaydın olmayabileceği durumlar için null kullanılabilir. Veritabanı sorgusu sonucunda beklenen bir sonuç bulunamadığında, bu durumu null olarak işaretleyebilirsiniz.
    
        Opsiyonel değerler ve güvenliğin sağlanması: Null, programın daha güvenli hale gelmesini sağlayabilir. Özellikle, bir değişkenin null olabileceğini belirtmek, programcının hata yapma riskini azaltabilir. Kotlin gibi modern dillerde, null güvenliği doğrudan dilin yapısına dahil edilmiştir ve null referans hatalarını önler.
    
    Ancak, null kullanımının kötü bir şekilde yönetilmesi veya aşırı kullanılması, kodun karmaşık hale gelmesine ve hata ayıklamanın zorlaşmasına neden olabilir. Bu nedenle, null kullanımını dikkatli bir şekilde düşünmek ve uygun durumlarda kullanmak önemlidir.

--- src/main/kotlin/Null.kt
+++ src/main/kotlin/Null.kt
@@ -1,2 +1,14 @@
 package org.example
 
+fun main() {
+    var name: String? ="Erkan"
+    var age: Int? = null
+    if (age != null){
+        println("$name is $age years old.")
+    }else{
+        println("Yaş Bilgisi Bulunamadı")
+    }
+    name = null
+    age = 22
+    println("Kullanıcı Adı: $name, Yaş : $age")
+}
